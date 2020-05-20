2d7d77ac2ee5bb5eb5c02e88a7554123023bcb05
d.porollo <d.porollo@gmail.com>
2020-05-20 17:45:34 +0300

    Added UDP Listener

--- src/sensor/sensor.go
+++ src/sensor/sensor.go
@@ -3,23 +3,25 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 )
 
 const (
 	network    = "udp"
 	address    = ":9995"
-	readBuffer = 1500
+	readBuffer = 1048576
 )
 
 func main() {
 
 	addr, _ := net.ResolveUDPAddr(network, address)
 	sock, _ := net.ListenUDP(network, addr)
+	sock.SetReadBuffer(readBuffer)
 
 	i := 0
 	for {
 		i++
-		buf := make([]byte, readBuffer)
+		buf := make([]byte, 1500)
 		rlen, _, err := sock.ReadFromUDP(buf)
 
 		if err != nil {
@@ -34,5 +36,24 @@ func main() {
 }
 
 func savePackets(buf []byte, rlen int) {
-	print("Hello")
+
+	f, err := os.Create("test.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	l, err := f.WriteString("Hello World")
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+
+	fmt.Println(l, "bytes written successfully")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
