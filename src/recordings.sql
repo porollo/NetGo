9bfa60786810ccaffbe39fc7461d891f45d9f933
porollo <d.porollo@gmail.com>
2022-09-07 00:21:08 +0300

    DbWorks

--- src/recordings.sql
+++ src/recordings.sql
@@ -2,9 +2,15 @@ DROP TABLE IF EXISTS netflow;
 
 CREATE TABLE netflow5 (
                        id         INT AUTO_INCREMENT NOT NULL,
-                       title      VARCHAR(128) NOT NULL,
-                       artist     VARCHAR(255) NOT NULL,
-                       price      DECIMAL(5,2) NOT NULL,
+                       version      VARCHAR(128) NOT NULL,
+                       count     VARCHAR(255) NOT NULL,
+                       sysUptime      DECIMAL(5,2) NOT NULL,
+                       unixSecs      DECIMAL(5,2) NOT NULL,
+                       unixNsecs      DECIMAL(5,2) NOT NULL,
+                       flowSequence      DECIMAL(5,2) NOT NULL,
+                       engineType      DECIMAL(5,2) NOT NULL,
+                       engineId      DECIMAL(5,2) NOT NULL,
+                       samplingInterval      DECIMAL(5,2) NOT NULL,
                        PRIMARY KEY (`id`)
 );
 
