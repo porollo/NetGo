3ef460bca00fbd23013ec1197e94cd1da149f009
porollo <d.porollo@gmail.com>
2022-09-07 14:23:45 +0300

    DbWorks

--- src/go.mod
+++ src/go.mod
@@ -4,10 +4,13 @@ go 1.19
 
 require (
 	github.com/jinzhu/gorm v1.9.16
-	gorm.io/gorm v1.23.8
+	gorm.io/driver/mysql v1.3.6
 )
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	gorm.io/gorm v1.23.8 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,25 +1,33 @@
 package main
 
+import (
+	"github.com/jinzhu/gorm"
+	"gorm.io/driver/mysql"
+)
+
 type Product struct {
+	gorm.Model
 	Code  string
 	Price uint
 }
 
 func main() {
-	db, err := Open("mssql", "sqlserver://gonf:gonf@localhost:1433?database=gonf")
+	db, err := gorm.Open(mysql.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
-	defer db.Close()
 
 	db.AutoMigrate(&Product{})
-
-	db.Create(&Product{Code: "L1212", Price: 1000})
+	db.Create(&Product{Code: "D42", Price: 100})
 
 	var product Product
-	db.First(&product, 1)                   // find product with id 1
-	db.First(&product, "code = ?", "L1212") // find product with code l1212
-	db.Model(&product).Update("Price", 2000)
+	db.First(&product, 1)
+	db.First(&product, "code = ?", "D42")
 
-	db.Delete(&product)
+	db.Model(&product).Update("Price", 200)
+
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	db.Delete(&product, 1)
 }
